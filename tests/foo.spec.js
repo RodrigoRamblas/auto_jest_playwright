3ae664c138bf5d80403a8059a5c0b73659468d22
Rodrigo Ramblas <rodrigo_ramblas@hotmail.com>
2021-07-29 10:17:06 -0300

    Primeiro Commit

---
+++ tests/foo.spec.js
@@ -0,0 +1,16 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Acessar o site "https://www.bempromotora.com.br/"', () => {
+  test('one', async ({ page }) => {
+    const site = 'https://www.bempromotora.com.br/';
+    await page.goto(site);
+    expect(page.url()).toBe('https://www.bempromotora.com.br/');
+    await page.click('xpath=//a[contains(.,´CARTÃO CONSIGNADO´)]');
+    expect(await page.url()).toBe('https://www.bempromotora.com.br/cartao-consignado');
+    expect(await page.isVisible('text=Cartão Consignado')).toBeTruthy();
+  });
+
+  test('two', async ({ page }) => {
+    // ...
+  });
+});
